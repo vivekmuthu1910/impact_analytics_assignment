336dca29cd1e8ac26b842484d51e201d983356bb
Vivekanandan M <vivekmuthu1910@gmail.com>
2021-10-22 09:34:43 +0530

    Converted power to shift

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 CONSEC_DAYS = 4
 
 
-@lru_cache(maxsize=100)
+@lru_cache(maxsize=1000)
 def count_invalids(n: int):
     if n < CONSEC_DAYS:
         return 0
@@ -13,9 +13,9 @@ def count_invalids(n: int):
     for i in range(n - CONSEC_DAYS + 1):
         current_values = 1
         if i < n - CONSEC_DAYS:
-            current_values *= 2 ** (n - CONSEC_DAYS - i)
+            current_values *= 1 << (n - CONSEC_DAYS - i)
         if i > 1:
-            current_values *= (2 ** (i - 1)) - count_invalids(i - 1)
+            current_values *= (1 << (i - 1)) - count_invalids(i - 1)
         total += current_values
     return total
 
@@ -26,14 +26,14 @@ def main():
     except ValueError:
         print("Enter a valid number")
         exit(1)
-    total = 2 ** n
-    valid = 2 ** n - count_invalids(n)
-    missed = int(total / 2)
+    total = 1 << n
+    valid = total - count_invalids(n)
+    missed = total >> 1
     missed -= count_invalids(n - 1)
     if n == CONSEC_DAYS:
         missed -= 1
     elif n > CONSEC_DAYS:
-        missed -= 2 ** (n - CONSEC_DAYS - 1) - count_invalids(n - CONSEC_DAYS - 1)
+        missed -= (1 << (n - CONSEC_DAYS - 1)) - count_invalids(n - CONSEC_DAYS - 1)
     print(f"{missed} / {valid}")
 
 
